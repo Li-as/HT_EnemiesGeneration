3461192809da67d26bc6afb8a1c8f368004023dc
Lias <haseo.vr@gmail.com>
2020-07-14 22:13:19 +0300

    Init

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Enemy _enemy;
+    [SerializeField] private float _spawnFrequency;
+    [SerializeField] private Transform _spawnPoints;
+
+    private float _runningTime;
+
+
+    private void Update()
+    {
+        _runningTime += Time.deltaTime;
+        if (_runningTime >= _spawnFrequency)
+        {
+            Vector3 newPositon = _spawnPoints.GetChild(Random.Range(0, _spawnPoints.childCount)).position;
+            Instantiate(_enemy, newPositon, Quaternion.identity);
+            _runningTime = 0;
+        }
+    }
+}
